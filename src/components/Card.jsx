636abfba50a319f160ca727af345417f429b015d
navin <navinexpo2020@gmail.com>
2024-02-28 22:14:43 +0530

    Added card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+// import React from "react";
 import { motion } from "framer-motion";
 import { LuFileSpreadsheet } from "react-icons/lu";
 import { FaDownload } from "react-icons/fa6";
